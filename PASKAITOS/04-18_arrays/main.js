0a4f9e2b947df5f502487c4cc6897e9d39f61fdd
simonasablinaite <simonasablinaite@gmail.com>
2022-04-20 09:57:55 +0300

    array methods

--- PASKAITOS/04-18_arrays/main.js
+++ PASKAITOS/04-18_arrays/main.js
@@ -1,41 +1,96 @@
+// elementu (stringu) isdestymas pagal abecele:
+
+const newArray = ['b', 'f', 'i', 'z', 'a'];
+console.log(newArray.sort());
+
+// elementu (skaiciu) isdestymas didejancia tvarka:
+
+const points = [40, 100, 5, 17, 1, 44]
+console.log(points.sort(function (a, b) { return a - b }));
+
+
+// todo ir done listu sukurimas ir panaudojimas:
+
+// issikeliame sukurtus listus is HTML'o:
+
 const todo = document.getElementById('todo');
 const done = document.getElementById('done');
-const inputElement = document.getElementById('input');
-const value = isValueEmptyOrExsisting
 
-let doneArray = [];
-let todoArray = ['Learn HTML', 'Learn CSS', 'Learn JavaScript']
+// susikuriame kintama 'todo' masyva:
+
+let todoArray = ['learn HTML', 'learn CSS', 'leatn Javascript', 'learn Node.js'];
+// kadangi kintamas todo masyvas suskurtas, reikia susikurti kintama done masyva su neapibreztomis reiksmemis:
+
+let doneArray = ['rest'];
+
+// isdestome elementus stulpeliu:
 
 todo.innerHTML = todoArray.join('<br>');
 
+// paieska su indexOf:
+
+console.log(todoArray.indexOf('learn CSS'));
+
+// is todo listo perkeliame elementus i done lista:
+
+
+const inputElement = document.getElementById('input');
+
+// tada susikuriame f-ja:
+
 function transferDone() {
-    doneArray = todoArray.splice(0, 2);
-    todoArray = todoArray.splice(0, 1);
+    // doneArray = todoArray.slice(0, 2);
+    // todoArray = todoArray.splice(2, 3);
+}
+transferDone();
 
-    updateList();
+
+function addNewTodo() {
+    todoArray.push(inputElement.value);
+    updateLists();
 }
 
-function addTodo() {
-    const value = inputElement.value
-    const isValueEmptyOrExsisting = value !== '' || !todoArray.includes(value);
+// tam, kad nebutu pridedami tusti ar pasikartojantys elementai, galima panaudoti f-ja:
 
-    if (!isValueEmptyOrExsisting) {
+function addNewTodo() {
+    const value = inputElement.value;
+    const isValueEmptyExisting = value != '' && !todoArray.includes(value);
+
+    if (isValueEmptyExisting) {
         todoArray.push(inputElement.value);
-        inputElement.value = '';
-        updateList();
+        inputElement.value = ''; // nurodome, kad istrintu naujai pridetus iteam'sus
+        updateLists();
     }
-
 }
 
-function updateList() {
+
+function updateLists() {
     todo.innerHTML = '';
+    done.innerHTML = '';
 
     for (let index = 0; index < todoArray.length; index++) {
-        todo.innerHTML += `<div>${todoArray[index]}</div>`
+
+        const listElement = document.createElement('div');
+        listElement.textContent = todoArray[index];
+
+        listElement.onclick = () => {
+            doneArray.push(todoArray.splice(index, 1));
+            updateLists;
+        }
+        todo.appendChild(listElement);
+        // todo.innerHTML += `<div>${todoArray[index]}</div>`
     }
 
+    for (let index = 0; index < done.length; index++) {
+        const listElement = document.createElement('div');
+        listElement.textContent = doneArray[index];
 
-    done.innerHTML = doneArray.join('<br>');
-    todo.innerHTML = todoArray.join('<br>');
+        listElement.onclick = () => {
+            todoArray.push(doneArray.splice(index, 1));
+            updateLists;
+
+            // done.inner += `<div>${doneArray[index]}</div>`
+        }
+        done.appendChild(listElement);
+    }
 }
-transferDone()
